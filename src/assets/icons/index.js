c88025fd323aa4e7f9a6e9419fe85804fe25bfe3
3ddyBoi <edvardhoiby@outlook.com>
2021-05-26 18:41:11 +0200

    Lagde toppen
    Lagde toppen på siden (head), her er det lokasjon og sosiale medier lett tilgjengelig.

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Envelope } from './Envelope'
+export { default as Instagram } from './Instagram'
+export { default as MapMarker } from './MapMarker'
+export { default as Snapchat } from './Snapchat'
