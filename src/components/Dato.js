094bc8d72145963906f357f35a9472a6e1ca2e2c
3ddyBoi <edvardhoiby@outlook.com>
2021-05-27 21:07:39 +0200

    La til en dato comp
    La til en dato comp, denne er ikke ferdig i det hele tatt enda.

---
+++ src/components/Dato.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/dato.css'
+
+function Dato() {
+    return (
+        <div className="grid">
+            <p className="festival">Festival</p>
+            <div className="box">
+                <p>
+                    10
+                </p>
+            </div>
+            <p className="juni">Juni</p>
+        </div>
+    )
+}
+
+export default Dato
