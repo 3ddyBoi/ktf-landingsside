6b7855bfea5505acce614a71dc50b74ccfaecc3e
3ddyBoi <edvardhoiby@outlook.com>
2021-05-27 21:05:32 +0200

    La til logo i Nav
    Jeg la til logo i nav, endret margins for at det skulle passe bedre resten av nettsiden og endret navn på ene tabben fra Kattas Teknologiske Festival til Festivalen.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import logo_lang from '../assets/images/logo_lang.svg'
 import '../styles/nav.css'
 
 function Nav() {
     return (
         <nav>
-            <a className="Logo" href="vg.no">
-                LOGO
+            <a className="logo" href="vg.no">
+                <img src={logo_lang} alt="Logo Kattas Teknologiske Festival" />
             </a>
             <a className="Kattas-Teknologiske-Festival" href="vg.no">
-                Kattas Teknologiske Festival
+                Festivalen
             </a>
             <a className="Pamelding" href="vg.no">
                 Påmelding
