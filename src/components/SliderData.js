e08b2218a79d87902fe4bb42ecfbd2dc1b817c76
3ddyBoi <edvardhoiby@outlook.com>
2021-05-28 13:10:21 +0200

    Fikset slider.
    Fikset slider, fikk linket til lokale bilder med json objeket. Endret bildelokasjoner for sliderbildene så det er mer sortert.

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,18 +1,19 @@
+import fire_gutter_ktt from '../assets/images/Sliderbilder/fire_gutter_ktt.jpg';
+import Jente_html from '../assets/images/Sliderbilder/Jente_html.jpg';
+import KTT_Jobbe_Caps from '../assets/images/Sliderbilder/KTT_Jobbe_Caps.jpg';
+import VR_briller from '../assets/images/Sliderbilder/VR_briller.jpg';
+
 export const SliderData = [
     {
-      image:
-        'https://images.unsplash.com/photo-1551244072-5d12893278ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80'
+      image: fire_gutter_ktt
     },
     {
-      image:
-      'https://images.unsplash.com/photo-1551058624-e9390c71d17d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80'
+      image: Jente_html
     },
     {
-      image:
-      'https://images.unsplash.com/photo-1551908325-9bd4c5c33171?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1190&q=80'
+      image: KTT_Jobbe_Caps 
     },
     {
-      image:
-      'https://images.unsplash.com/photo-1553152531-b98a2fc8d3bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80'
+      image: VR_briller
     }
   ];
